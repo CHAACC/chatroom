8f263558338726a48920a304be42a7243b91f50b
pengkaiyang <476922814@qq.com>
2019-07-19 00:21:13 +0800

    //yaojiang.top/

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 // export const SERVER_URL = '//localhost:7001/'
-export const SERVER_URL = '//yaojiang.top:7001/'
+export const SERVER_URL = '//yaojiang.top/'
 
 export const QN_DOMAIN = 'img.yaojiang.top'
