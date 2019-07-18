c1863b35999dd8c76a7c2474e9ebc41280b258bd
pengkaiyang <476922814@qq.com>
2019-07-18 23:33:50 +0800

    修改SERVER_URL为//

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-// export const SERVER_URL = 'http://localhost:7001/'
-export const SERVER_URL = 'https://yaojiang.top:7001/'
+// export const SERVER_URL = '//localhost:7001/'
+export const SERVER_URL = '//yaojiang.top:7001/'
 
 export const QN_DOMAIN = 'img.yaojiang.top'
