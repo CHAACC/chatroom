7fa842db50eb60b411a93b037be17ef419ad5cc9
pengkaiyang <476922814@qq.com>
2019-07-18 12:15:52 +0800

    使用域名

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 // export const SERVER_URL = 'http://localhost:7001/'
-export const SERVER_URL = 'http://182.61.35.13:7001/'
+export const SERVER_URL = 'http://yaojiang.top:7001/'
 
-export const QN_DOMAIN = 'ps60h08zx.bkt.clouddn.com'
+export const QN_DOMAIN = 'img.yaojiang.top'
